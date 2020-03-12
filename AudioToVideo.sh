846fb26d35101c099f4823a2216a7da1f668e8ff
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-03-12 19:25:46 +0100

    (-_-)
    - Text to display
    - Add TODO tasks
    - Set a faster compression

--- AudioToVideo.sh
+++ AudioToVideo.sh
@@ -3,6 +3,10 @@
 # Create a slideshow or still image video for YouTube from an audio file,
 # image and text input with a waveform animation.
 
+# TODO BY default display artist and title
+# TODO Set option to display title only
+# TODO Extract Artist and Title from audio file instead of specify it, unless there is nothing in the file
+
 # Assuming this program is running on a POSIX platform
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     FFMPEG_EXE=ffmpeg.sh
@@ -33,17 +37,17 @@ else
     -font Helvetica-Bold -gravity Center -weight 700 -pointsize 100 caption:"$ARTIST\n$TITLE" $IMG_TEXT
     # convert image to full hd for video
     convert -gravity Center -resize 1920x1080^ -extent 1920x1080 "$1" $COVER_HD && \
-    echo $COVER_HD generated
+    echo "$COVER_HD generated"
     # merge transparent images for video
     composite -dissolve 50 -gravity Center $IMG_TEXT $COVER_HD -alpha Set $VIDEO_IMG && \
-    echo $VIDEO_IMG generated
+    echo "$VIDEO_IMG generated"
     #convert image to square for SoundCloud and Insta
     convert -gravity Center -resize 1080x1080^ -extent 1080x1080 $VIDEO_IMG $COVER_SND && \
-    echo $COVER_SND generated
+    echo "$COVER_SND generated"
     # genreate eq video
     time $FFMPEG_EXE -i "$2" -loop 1 -i $VIDEO_IMG \
     -filter_complex "[0:a]showwaves=s=1920x200:mode=cline:colors=0xFFFFFF|0xD3D3D3:scale=sqrt[fg];[1:v]scale=1920:-1[bg];[bg][fg]overlay=shortest=1:850:format=auto,format=yuv420p[out]" \
-    -map "[out]" -map 0:a -pix_fmt yuv420p -c:v libx264 -preset medium -crf 18 -c:a copy -shortest $VIDEO_FILE && \
-    echo $VIDEO_FILE generated
+    -map "[out]" -map 0:a -pix_fmt yuv420p -c:v libx264 -preset fast -crf 18 -c:a copy -shortest $VIDEO_FILE && \
+    echo "$VIDEO_FILE generated"
     rm -v $IMG_TEXT $VIDEO_IMG
 fi
