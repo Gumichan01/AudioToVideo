acc08438f87ba81b8d58fe34c7c04d7c82c97b14
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-03-12 18:42:25 +0100

    Improve the behaviour of the existing code.
    1. Extract values and define variable with explicit name
    2. Notify the user when a step is done properly
    3. Remove temporary files

--- AudioToVideo.sh
+++ AudioToVideo.sh
@@ -5,34 +5,45 @@
 
 # Assuming this program is running on a POSIX platform
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    FFMPEG_EXE=ffmpeg
+    FFMPEG_EXE=ffmpeg.sh
 elif [[ "$OSTYPE" == "darwin*" ]]; then
-    FFMPEG_EXE=/Applications/ffmpeg
+    FFMPEG_EXE=/Applications/ffmpeg     # Is that really necessary?
 else
     echo "Invalid OS - It must be a POSIX platform (Linux, Mac)"
     exit 1
 fi
 
+IMAGE_FILE=$1
+AUDIO_FILE=$2
+ARTIST=$3
+TITLE=$4
 
-if [ -z "$1" ] && [ -z "$2" ] && [ -z "$3" ] && [ -z "$4" ]; then
+IMG_TEXT=image-text.png
+COVER_HD="$ARTIST"-"$TITLE"-cover-hd.png
+COVER_SND="$ARTIST"-"$TITLE"-cover-snd.png
+VIDEO_IMG=video-img.png
+VIDEO_FILE="$ARTIST"-"$TITLE"-video.mkv
+
+if [ -z "$IMAGE_FILE" ] && [ -z "$AUDIO_FILE" ] && [ -z "$ARTIST" ] && [ -z "$TITLE" ]; then
     echo Syntax:
-    echo `basename $0`" /path/to/image.jpg /path/to/audio.mp3 \"artist name\" \"Track Title\""
+    echo `basename $0`" /path/to/image.jpg /path/to/audio_file \"artist name\" \"Track Title\""
 else
     # generate text image for video
-    convert -gravity southeast -splice 40x40  -gravity northwest -splice 40x40 \
-    -font Helvetica-Bold -gravity Center -weight 700 -pointsize 100 caption:"$3\n$4" image-text.png
+    convert -gravity southeast -splice 40x40 -gravity northwest -splice 40x40 \
+    -font Helvetica-Bold -gravity Center -weight 700 -pointsize 100 caption:"$ARTIST\n$TITLE" $IMG_TEXT
     # convert image to full hd for video
-    convert -gravity Center -resize 1920x1080^ -extent 1920x1080 "$1" "$3"-"$4"-1920x1080.png
+    convert -gravity Center -resize 1920x1080^ -extent 1920x1080 "$1" $COVER_HD && \
+    echo $COVER_HD generated
     # merge transparent images for video
-    composite -dissolve 50 -gravity Center image-text.png "$3"-"$4"-1920x1080.png -alpha Set "$3"-"$4"-cover-1920x1080.png
+    composite -dissolve 50 -gravity Center $IMG_TEXT $COVER_HD -alpha Set $VIDEO_IMG && \
+    echo $VIDEO_IMG generated
     #convert image to square for SoundCloud and Insta
-    convert -gravity Center -resize 1080x1080^ -extent 1080x1080 "$3"-"$4"-cover-1920x1080.png "$3"-"$4"-cover-1080x1080.png
+    convert -gravity Center -resize 1080x1080^ -extent 1080x1080 $VIDEO_IMG $COVER_SND && \
+    echo $COVER_SND generated
     # genreate eq video
-    $FFMPEG_EXE -i "$2" -loop 1 -i "$3"-"$4"-cover-1920x1080.png \
-    -filter_complex "[0:a]showwaves=s=1920x200:mode=cline:colors=0xFFFFFF|0xD3D3D3:scale=sqrt:draw=full[fg];[1:v]scale=1920:-1[bg];[bg][fg]overlay=shortest=1:850:format=auto,format=yuv420p[out]" \
-    -map "[out]" -map 0:a -pix_fmt yuv420p -c:v libx264 -preset medium -crf 18 -c:a copy -shortest "$3"-"$4"-video.mkv && \
-    echo "$3"-"$4"-1920x1080.png genrated && \
-    echo "$3"-"$4"-cover-1920x1080.png generated && \
-    echo "$3"-"$4"-cover-1080x1080.png generated && \
-    echo "$3"-"$4"-video.mkv generated
+    time $FFMPEG_EXE -i "$2" -loop 1 -i $VIDEO_IMG \
+    -filter_complex "[0:a]showwaves=s=1920x200:mode=cline:colors=0xFFFFFF|0xD3D3D3:scale=sqrt[fg];[1:v]scale=1920:-1[bg];[bg][fg]overlay=shortest=1:850:format=auto,format=yuv420p[out]" \
+    -map "[out]" -map 0:a -pix_fmt yuv420p -c:v libx264 -preset medium -crf 18 -c:a copy -shortest $VIDEO_FILE && \
+    echo $VIDEO_FILE generated
+    rm -v $IMG_TEXT $VIDEO_IMG
 fi
