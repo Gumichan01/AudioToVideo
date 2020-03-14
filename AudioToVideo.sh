40b24d9804b7ef563ba6104e5d2e3bb06b258a1b
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-03-14 16:39:04 +0100

    Extract every complex commands in new functions

--- AudioToVideo.sh
+++ AudioToVideo.sh
@@ -3,12 +3,13 @@
 # Create a slideshow or still image video for YouTube from an audio file,
 # image and text input with a waveform animation.
 
-# TODO add options: --artist, --title, --waves
 # TODO Specify the colours of the waveform
 
 # Configuration
-FFMPEG_EXE=ffmpeg.sh
+FFMPEG_EXE=ffmpeg
 TMP_METADATA_FILE=/tmp/$(echo ${$})-metadata.txt
+EXIT_SUCCESS=0
+EXIT_FAILURE=1
 
 IMAGE_FILE=$1
 AUDIO_FILE=$2
@@ -30,31 +31,52 @@ extract_metadata () {
     COVER_HD=${ARTIST}-${TITLE}-cover-hd.png && \
     COVER_SND=${ARTIST}-${TITLE}-cover-snd.png && \
     VIDEO_FILE=${ARTIST}-${TITLE}-video.mkv && \
-    rm -f $TMP_METADATA_FILE && \
     return 0 || return 1
+}
+
+generate_full_hd_text_image_for_video () {
+    convert -gravity southeast -splice 40x40 -gravity northwest -splice 40x40 \
+    -font Helvetica-Bold -gravity Center -weight 700 -pointsize 100 caption:"$ARTIST\n$TITLE" $IMG_TEXT && \
+    convert -gravity Center -resize 1920x1080^ -extent 1920x1080 "$IMG_TEXT" $COVER_HD && \
+    echo "$COVER_HD generated" && \
+    return 0 || return 1
+}
+
+merge_text_and_cover_for_video () {
+    composite -dissolve 50 -gravity Center $IMG_TEXT $COVER_HD -alpha Set $VIDEO_IMG && \
+    echo "$VIDEO_IMG generated" && \
+    return 0 || return 1
+}
+
+convert_hd_image_for_soundcloud_cover () {
+    convert -gravity Center -resize 1080x1080^ -extent 1080x1080 $VIDEO_IMG $COVER_SND && \
+    echo "$COVER_SND generated" && \
+    return 0 || return 1
+}
+
+generate_video () {
+    $FFMPEG_EXE -i "$AUDIO_FILE" -loop 1 -i $VIDEO_IMG \
+    -filter_complex "[0:a]showwaves=s=1920x200:mode=cline:colors=0xFFFFFF|0xD3D3D3:scale=sqrt[fg];[1:v]scale=1920:-1[bg];[bg][fg]overlay=shortest=1:850:format=auto,format=yuv420p[out]" \
+    -map "[out]" -map 0:a -pix_fmt yuv420p -c:v libx264 -preset fast -crf 18 -c:a copy -shortest $VIDEO_FILE && \
+    echo "$VIDEO_FILE generated" && \
+    return 0 || return 1
+}
+
+audeo () {
+    extract_metadata && \
+    generate_full_hd_text_image_for_video && \
+    merge_text_and_cover_for_video && \
+    convert_hd_image_for_soundcloud_cover && \
+    generate_video
+    local AUDEO_RESULT=$?
+    rm -f $TMP_METADATA_FILE
+    rm -vf $IMG_TEXT $VIDEO_IMG
+    return $AUDEO_RESULT
 }
 
 if [ -z "$IMAGE_FILE" ] && [ -z "$AUDIO_FILE" ]; then
     echo Syntax:
     echo `basename $0`" /path/to/image.[png|jpg] /path/to/audio_file.[flac|mp3|wav|ogg]"
 else
-    extract_metadata
-    # generate text image for video
-    convert -gravity southeast -splice 40x40 -gravity northwest -splice 40x40 \
-    -font Helvetica-Bold -gravity Center -weight 700 -pointsize 100 caption:"$ARTIST\n$TITLE" $IMG_TEXT
-    # convert image to full hd for video
-    convert -gravity Center -resize 1920x1080^ -extent 1920x1080 "$1" $COVER_HD && \
-    echo "$COVER_HD generated"
-    # merge transparent images for video
-    composite -dissolve 50 -gravity Center $IMG_TEXT $COVER_HD -alpha Set $VIDEO_IMG && \
-    echo "$VIDEO_IMG generated"
-    #convert image to square for SoundCloud and Insta
-    convert -gravity Center -resize 1080x1080^ -extent 1080x1080 $VIDEO_IMG $COVER_SND && \
-    echo "$COVER_SND generated"
-    # generate eq video
-    #time $FFMPEG_EXE -i "$2" -loop 1 -i $VIDEO_IMG \
-    #-filter_complex "[0:a]showwaves=s=1920x200:mode=cline:colors=0xFFFFFF|0xD3D3D3:scale=sqrt[fg];[1:v]scale=1920:-1[bg];[bg][fg]overlay=shortest=1:850:format=auto,format=yuv420p[out]" \
-    #-map "[out]" -map 0:a -pix_fmt yuv420p -c:v libx264 -preset fast -crf 18 -c:a copy -shortest $VIDEO_FILE && \
-    #echo "$VIDEO_FILE generated"
-    rm -v $IMG_TEXT $VIDEO_IMG
+    audeo
 fi
