2241c6da3c6073bc9678b0ab2503942b09e4d3a4
Gumichan01 <luxon.jean.pierre@gmail.com>
2020-03-14 23:55:24 +0100

    Fix bad cover image setting
    + extract a piece of code

--- AudioToVideo.sh
+++ AudioToVideo.sh
@@ -30,33 +30,37 @@ extract_metadata () {
 
     if [ -z "$ARTIST" ] || [ -z "$TITLE" ]; then
         echo -e "A metadata field is missing in \"$AUDIO_FILE\": artist or title" 1>&2
-        return 1
+        return $EXIT_FAILURE
     else
         COVER_HD=${ARTIST}-${TITLE}-cover-hd.png
         COVER_SND=${ARTIST}-${TITLE}-cover-snd.png
         VIDEO_FILE=${ARTIST}-${TITLE}-video.mkv
-        return 0
+        return $EXIT_SUCCESS
     fi
 }
 
 generate_full_hd_text_image_for_video () {
     convert -gravity southeast -splice 40x40 -gravity northwest -splice 40x40 \
     -font Helvetica-Bold -gravity Center -weight 700 -pointsize 100 caption:"$ARTIST\n$TITLE" $IMG_TEXT && \
-    convert -gravity Center -resize 1920x1080^ -extent 1920x1080 "$IMG_TEXT" $COVER_HD && \
+    return $EXIT_SUCCESS || return $EXIT_FAILURE
+}
+
+generate_cover_image_for_video () {
+    convert -gravity Center -resize 1920x1080^ -extent 1920x1080 "$IMAGE_FILE" $COVER_HD && \
     echo "$COVER_HD generated" && \
-    return 0 || return 1
+    return $EXIT_SUCCESS || return $EXIT_FAILURE
 }
 
 merge_text_and_cover_for_video () {
     composite -dissolve 50 -gravity Center $IMG_TEXT $COVER_HD -alpha Set $VIDEO_IMG && \
     echo "$VIDEO_IMG generated" && \
-    return 0 || return 1
+    return $EXIT_SUCCESS || return $EXIT_FAILURE
 }
 
 convert_hd_image_for_soundcloud_cover () {
     convert -gravity Center -resize 1080x1080^ -extent 1080x1080 $VIDEO_IMG $COVER_SND && \
     echo "$COVER_SND generated" && \
-    return 0 || return 1
+    return $EXIT_SUCCESS || return $EXIT_FAILURE
 }
 
 generate_video () {
@@ -64,12 +68,13 @@ generate_video () {
     -filter_complex "[0:a]showwaves=s=1920x200:mode=cline:colors=0xFFFFFF|0xD3D3D3:scale=sqrt[fg];[1:v]scale=1920:-1[bg];[bg][fg]overlay=shortest=1:850:format=auto,format=yuv420p[out]" \
     -map "[out]" -map 0:a -pix_fmt yuv420p -c:v libx264 -preset fast -crf 18 -c:a copy -shortest $VIDEO_FILE && \
     echo "$VIDEO_FILE generated" && \
-    return 0 || return 1
+    return $EXIT_SUCCESS || return $EXIT_FAILURE
 }
 
 audeo () {
     extract_metadata && \
     generate_full_hd_text_image_for_video && \
+    generate_cover_image_for_video && \
     merge_text_and_cover_for_video && \
     convert_hd_image_for_soundcloud_cover && \
     generate_video
